b84be22daa8e9b78d103628fff7ffcd4f4b83aa6
exerex <exerex11@gmail.com>
2020-04-06 11:25:36 -0300

    Updates

---
+++ precache-manifest.663035e9d3fe8618387e70cc666943e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e178db207b1e57bbc3c5b0f59c810f70",
+    "url": "/react-covid/index.html"
+  },
+  {
+    "revision": "14777180635f65b7b2d0",
+    "url": "/react-covid/static/css/main.c2fbcd62.chunk.css"
+  },
+  {
+    "revision": "f9441e3a97902bd425e7",
+    "url": "/react-covid/static/js/2.ec9efa9f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid/static/js/2.ec9efa9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14777180635f65b7b2d0",
+    "url": "/react-covid/static/js/main.d6320200.chunk.js"
+  },
+  {
+    "revision": "b9c5a219cfa8fb5cebf1",
+    "url": "/react-covid/static/js/runtime-main.f0992da3.js"
+  },
+  {
+    "revision": "1270cc4d2dfe54c565a9aab500333c13",
+    "url": "/react-covid/static/media/image-front.1270cc4d.png"
+  }
+]);
